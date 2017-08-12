c4dc19ea12a289f03eb1bac2572bddb2a7a47d83
Satoshi OKANO <satokano@gmail.com>
2017-08-13 00:26:58 +0900

    add logger

--- kabaobot/funcviews.py
+++ kabaobot/funcviews.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import permissions
 from rest_framework.response import Response
+import logging
 
 @api_view(('GET',))
 @permission_classes((permissions.AllowAny,))
@@ -10,5 +11,13 @@ def hello_world(request):
 @api_view(('GET','POST',))
 @permission_classes((permissions.AllowAny,))
 def linecallback(request):
+    logger = logging.getLogger('linecallbacklogger')
+    logger.info("[LC] callback start")
+
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    logger.info("[LC] Request body: " + body)
+
+    logger.info("[LC] callback end")
     return Response("OK")
 
