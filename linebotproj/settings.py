bf4699b344bf00625da1321fb5c5ae4de333590b
Satoshi OKANO <satokano@gmail.com>
2017-08-13 15:08:30 +0900

    trailing ,

--- linebotproj/settings.py
+++ linebotproj/settings.py
@@ -165,8 +165,8 @@ CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 
 # Cloudinary
 cloudinary.config(
-    cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME', None)
-    api_key = os.getenv('CLOUDINARY_API_KEY', None)
-    api_secret = os.getenv('CLOUDINARY_API_SECRET', None)
+    cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME', None),
+    api_key = os.getenv('CLOUDINARY_API_KEY', None),
+    api_secret = os.getenv('CLOUDINARY_API_SECRET', None),
 )
 
