3f562325235ef83e1fc7ec5506752c83166d7a4f
Satoshi OKANO <satokano@gmail.com>
2017-08-19 03:25:47 +0900

    add LineUser model

--- kabaobot/models.py
+++ kabaobot/models.py
@@ -6,4 +6,14 @@ class Entry(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     body = models.TextField()
 
+class LineUser(models.Model):
+    user_id = models.CharField(max_length=64)
+    display_name = models.CharField(max_length=256)
+    picture_url = models.CharField(max_length=1024)
+    status_message = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.display_name
 
