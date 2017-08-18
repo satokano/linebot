dbbbdabff31fc52f10171944cdc516edc3d37f4f
Satoshi OKANO <satokano@gmail.com>
2017-08-19 04:31:24 +0900

    admin site

--- kabaobot/admin.py
+++ kabaobot/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class EntryAdmin(admin.ModelAdmin)
+    pass
+
+class LineUserAdmin(admin.ModelAdmin)
+    pass
+
+admin.site.register(Entry, EntryAdmin)
+admin.site.register(LineUser, LineUserAdmin)
+
